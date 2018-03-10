ea42c3464b5fbf372162a3c8fbe5166861247421
Angela <angela@jnickel.com>
2018-03-10 16:05:54 -0400

    added database

--- config/config.php
+++ config/config.php
@@ -4,25 +4,15 @@ use function DI\env;
 use function DI\get;
 
 return [
-    'appName' => 'HashPipe',
+    'appName' => 'GroceryImageParser',
     'displayErrorDetails' => true,
-    'hp.environment' => env('HP_ENVIRONMENT', 'local'),
-    'developmentMode' => env('DEVELOPMENT_MODE', true),
+    'environment.mode' => env('ENVIRONMENT_MODE', 'local'),
     'database' => [
         'driver' => 'mysqli',
         'host' => env("DB_HOST", "127.0.0.1"),
-        'user' => env("DB_USER", "hashpipe_admin"),
-        'password' => env("DB_PASSWORD", 'Ohka8ushiev6Mul8'),
-        'dbname' => env("DB_NAME", 'hashpipe'),
+        'user' => env("DB_USER", "grocery_admin"),
+        'password' => env("DB_PASSWORD", 'Ohyieph5Ri2ceetuop7aeGhaoja9Vied'),
+        'dbname' => env("DB_NAME", 'grocery_admin'),
         'port' => 3306
-    ],
-    'auth0' => [
-        'domain' => 'imposium.auth0.com',
-        'client_id' => 'ufhMqSk6yChekUeCcWK8d1A9EtzrY7cP',
-        'client_secret' => 'RZAc40PQBzr5HHjomfeB58nvvNTWeaij9tHLl9QCDNVlhJ_nEH8Y3v59GKRY9g-2',
-        'valid_audiences' => [
-            'ufhMqSk6yChekUeCcWK8d1A9EtzrY7cP', //API
-            'bcT4BfOY05vs65YziCoHTrkADCPLM5ra'  //Front end
-        ]
     ]
 ];
